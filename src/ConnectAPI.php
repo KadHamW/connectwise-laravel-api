19032e0e9cbf8d7153d71a485afbecf6684216a8
BuildTools <unconfigured@null.spigotmc.org>
2021-03-16 14:44:22 +1000

    Can now retrieve contacts from API

--- src/ConnectAPI.php
+++ src/ConnectAPI.php
@@ -4,10 +4,11 @@ namespace Kadhamw\ConnectAPI;
 
 use Kadhamw\ConnectAPI\API\Company;
 use Kadhamw\ConnectAPI\API\Opportunity;
+use Kadhamw\ConnectAPI\API\Contact;
 
 class ConnectAPI
 {
     public function test(){
-        return Opportunity::postOpportunity("Test");
+        return Contact::getContacts();
     }
 }
---
+++ src/api/Contact.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kadhamw\ConnectAPI\API;
+
+use Kadhamw\ConnectAPI\Connection;
+
+class Contact
+{
+    public static function getContactCount(){
+        $conn = new Connection();
+        $result = $conn->request('company/contacts/count');
+        return json_decode($result)->count;
+    }
+
+    public static function getContacts(){
+        $count = Contact::getContactCount();
+        $pages = (int)ceil($count / 100);
+        $tmp_contacts = [];
+        for ($i=1; $i <= $pages; $i++) {
+            $conn = new Connection();
+            $result = $conn->request('company/contacts/?pagesize=100&page='.$i);
+            $tmp_contacts = json_decode($result);
+            foreach($tmp_contacts as $tmp_contact){
+                $tmp_contacts[] = $tmp_contact;
+            }
+        }
+        return $tmp_contacts;
+    }
+}
