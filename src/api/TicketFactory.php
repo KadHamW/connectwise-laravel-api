54b9ff51b547a42f43f94b72653a485c572b1a81
kadhamw <kade@procomputers.com.au>
2021-06-17 10:48:21 +1000

    Fixed up ticket factory

--- src/api/TicketFactory.php
+++ src/api/TicketFactory.php
@@ -2,6 +2,7 @@
 
 namespace Kadhamw\ConnectAPI\API;
 
+use Kadhamw\ConnectAPI\API\TicketFactory as APITicketFactory;
 use Kadhamw\ConnectAPI\Connection;
 
 class TicketFactory
@@ -32,5 +33,53 @@ class TicketFactory
         $conn = new Connection();
         $result = $conn->request('/service/tickets/count');
         return json_decode($result)->count;
+    }
+
+    public static function getTickets(){
+        $count = TicketFactory::getTicketCount();
+        $pages = (int)ceil($count / 100);
+        $_tickets = [];
+        for ($i=1; $i <= $pages; $i++) {
+            $conn = new Connection();
+            $result = $conn->request('service/tickets/?pagesize=100&page='.$i);
+            $tmp_tickets = json_decode($result);
+            $_tickets[] = $tmp_tickets;
+            $_ticketsCollection = collect($tmp_tickets)->values()->all();
+        }
+        dd($_ticketsCollection);
+        return $_ticketsCollection;
+    }
+
+    public static function getTicket($id){
+        $conn = new Connection();
+        $result = $conn->request('service/tickets/'.$id);
+        $_ticket =  json_decode($result);
+        return $_ticket;
+    }
+    public static function getTicketActivities($id){
+        $conn = new Connection();
+        $result = $conn->request('service/tickets/'.$id.'/activities');
+        $_activities =  json_decode($result);
+        return $_activities;
+    }
+
+    public static function getTicketNotesCount($id){
+        $conn = new Connection();
+        $result = $conn->request('/service/tickets/'.$id.'/count');
+        return json_decode($result)->count;
+    }
+
+    public static function getTicketNotes($id){
+        $count = TicketFactory::getTicketNotesCount($id);
+        $pages = (int)ceil($count / 100);
+        $_ticket_notes = [];
+        for ($i=1; $i <= $pages; $i++) {
+            $conn = new Connection();
+            $result = $conn->request('service/tickets/'.$id.'/notes/?pagesize=100&page='.$i);
+            $tmp_ticket_notes = json_decode($result);
+            $_ticket_notes[] = $tmp_ticket_notes;
+        }
+
+        return $_ticket_notes;
     }
 }
